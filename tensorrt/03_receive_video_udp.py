3ef2c6cf9e95c2400000b00e89312afdb158043c
maximofn <maximofn@gmail.com>
2022-12-21 12:54:44 +0100

    Creo dos programas para enviar y recibir vídeo por udp

---
+++ tensorrt/03_receive_video_udp.py
@@ -0,0 +1,46 @@
+import cv2
+import socket
+import numpy as np
+
+# Creamos el socket UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Asignamos la dirección y puerto del socket
+sock.bind(('127.0.0.1', 8554))
+
+len_message = 0
+len_message_old = 0
+
+frame = []
+
+# Creamos un bucle infinito para recibir y mostrar el vídeo por el socket
+while True:
+    # Recibimos el mensaje del socket
+    message, addr = sock.recvfrom(65507)
+
+    # len_message = len(message)
+    # if len_message != len_message_old:
+    #     print(f"len_message: {len_message}, type: {type(len_message)}")
+    #     len_message_old = len_message
+
+    # Añadimos bytes al final del mensaje hasta que su tamaño sea divisible por 3
+    while len(message) % 65151 != 0:
+        message += b'\x00'
+
+    # Creamos un array NumPy a partir de la secuencia de bytes
+    frame = np.frombuffer(message, dtype=np.uint8)
+
+    # Redimensionamos el array NumPy a una imagen de 3 canales
+    frame = frame.reshape(127, 171, 3)
+    frame = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_NEAREST)
+
+    # Mostramos la imagen en una ventana
+    cv2.imshow('Video', frame)
+
+    # Si se presiona la tecla 'q', salimos del bucle
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Cerramos la ventana y el socket
+# cv2.destroyAllWindows()
+sock.close()
---
+++ tensorrt/03_send_video_udp.py
@@ -0,0 +1,50 @@
+import cv2
+import socket
+import numpy as np
+
+# Creamos el socket UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Abrimos la cámara
+cap = cv2.VideoCapture(0)
+# Full HD
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+len_frame = 0
+len_frame_old = 0
+
+running = True
+
+# Creamos un bucle infinito para enviar el vídeo por el socket
+while True:
+    # Leemos el frame de la cámara
+    ret, frame = cap.read()
+
+    # Si no hay frame, salimos del bucle
+    if not ret:
+        break
+
+    # Imprimimos el tamaño del frame
+    len_frame = len(frame.tobytes())
+    if len_frame != len_frame_old:
+        print(f"len_frame: {len_frame}, shape: {frame.shape}")
+        len_frame_old = len_frame
+
+    # Redimensionamos el array NumPy a un tamaño de 127x170
+    frame = cv2.resize(frame, (170, 127), interpolation=cv2.INTER_NEAREST)
+
+    # Display
+    # cv2.imshow("frame", frame)
+    # if cv2.waitKey(1) == ord('q'):
+    #     break
+
+    # Enviamos el frame por el socket en bloques de tamaño máximo permitido
+    message = frame.tobytes(order='C')
+    block_size = 65000  # Tamaño máximo de cada bloque
+    for i in range(0, len(message), block_size):
+        sock.sendto(message[i:i + block_size], ('127.0.0.1', 8554))
+
+# Cerramos el socket y la cámara
+sock.close()
+cap.release()
