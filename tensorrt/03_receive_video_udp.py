2d558431ee92b23e5c436020c00722ea9fff3adc
maximofn <maximofn@gmail.com>
2022-12-22 16:17:24 +0100

    Modifico los archivos de enviar y recibir video por UDP para poder mandarlo a cualquier calidad

--- tensorrt/03_receive_video_udp.py
+++ tensorrt/03_receive_video_udp.py
@@ -6,41 +6,68 @@ import numpy as np
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Asignamos la dirección y puerto del socket
-sock.bind(('127.0.0.1', 8554))
+sock.bind(('localhost', 8554))
 
 len_message = 0
 len_message_old = 0
 
 frame = []
+start_frame = False
+end_frame = False
+num_end_end_frame = 0
+resize = False
+image = False
 
 # Creamos un bucle infinito para recibir y mostrar el vídeo por el socket
 while True:
     # Recibimos el mensaje del socket
-    message, addr = sock.recvfrom(64770)#65507)
+    message, addr = sock.recvfrom(65000)
+    # print(f"len_message: {len(message)}")
 
-    len_message = len(message)
-    if len_message != len_message_old:
-        print(f"len_message: {len_message}, type: {type(len_message)}")
-        len_message_old = len_message
+    # len_message = len(message)
+    # if len_message != len_message_old:
+    #     print(f"len_message: {len_message}, type: {type(len_message)}")
+    #     len_message_old = len_message
 
-    # Añadimos bytes al final del mensaje hasta que su tamaño sea divisible por 3
-    # while len(message) % 65151 != 0:
-    #     message += b'\x00'
+    if len(message) == 10:
+        print("******************************************start frame")
+        start_frame = True
+        continue
 
     # Creamos un array NumPy a partir de la secuencia de bytes
-    frame = np.frombuffer(message, dtype=np.uint8)
+    if len(message) == 20:
+        print(f"--------------------------------------------end frame, len frame: {len(frame)}")
+        end_frame = True
+        if num_end_end_frame == 0:
+            num_end_end_frame += 1
+        elif num_end_end_frame == 1:
+            num_end_end_frame += 1
+    else:
+        if start_frame:
+            frame = np.frombuffer(message, dtype=np.uint8)
+            start_frame = False
+        else:
+            frame = np.append(frame, np.frombuffer(message, dtype=np.uint8))
+        print(f"len message: {len(message)}, len frame: {len(frame)}")
 
-    # Redimensionamos el array NumPy a una imagen de 3 canales
-    frame = frame.reshape(127, 170, 3)
-    frame = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_NEAREST)
+    # Si hemos recibido el frame completo, lo mostramos
+    if end_frame and num_end_end_frame > 1:
+        # Redimensionamos el array NumPy a una imagen de 3 canales
+        print(f"len frame: {len(frame)}")
+        frame = frame.reshape(720, 1280, 3)
 
-    # Mostramos la imagen en una ventana
-    cv2.imshow('Video', frame)
+        # # Redimensionamos el frame a un tamaño de 640x480
+        # if resize: frame = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_NEAREST)
+
+        # # Mostramos la imagen en una ventana
+        cv2.imshow('Video', frame)
+        
+        end_frame = False
 
     # Si se presiona la tecla 'q', salimos del bucle
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Cerramos la ventana y el socket
-# cv2.destroyAllWindows()
+cv2.destroyAllWindows()
 sock.close()
--- tensorrt/03_send_video_udp.py
+++ tensorrt/03_send_video_udp.py
@@ -1,13 +1,45 @@
 import cv2
 import socket
-import numpy as np
+import threading
+import sys
+import select
+import time
+
+# Creamos una clase que hereda de threading.Thread y sobrescribe el método
+# run(). Este método será el encargado de verificar la entrada de la terminal.
+class InputThread(threading.Thread):
+    data = None
+
+    def run(self):
+        # Configuramos el descriptor de archivo de la entrada de la terminal como stdin.
+        input_fd = sys.stdin.fileno()
+
+        # Usamos el método select.select() para ver si hay algún dato disponible
+        # para leer en la entrada de la terminal.
+        read_fds, _, _ = select.select([input_fd], [], [])
+
+        # Si read_fds contiene el descriptor de archivo de la entrada de la terminal,
+        # significa que hay algún dato disponible para leer. En ese caso, leemos el
+        # dato utilizando el método sys.stdin.readline().
+        if input_fd in read_fds:
+            self.data = sys.stdin.readline()
+    
+    def get_data(self):
+        return self.data
+    
+    def clear_data(self):
+        self.data = None
+
+
+# Creamos una instancia de la clase InputThread y la iniciamos.
+input_thread = InputThread()
+input_thread.start()
 
 # Creamos el socket UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Abrimos la cámara
+# Abrimos la cámara en full HD
 cap = cv2.VideoCapture(0)
-# Full HD
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
@@ -16,7 +48,8 @@ len_frame_old = 0
 len_resized_frame = 0
 len_resized_frame_old = 0
 
-running = True
+display = False
+resize = False
 
 # Creamos un bucle infinito para enviar el vídeo por el socket
 while True:
@@ -34,7 +67,7 @@ while True:
         len_frame_old = len_frame
 
     # Redimensionamos el array NumPy a un tamaño de 127x170
-    frame = cv2.resize(frame, (170, 127), interpolation=cv2.INTER_NEAREST)
+    if resize: frame = cv2.resize(frame, (170, 127), interpolation=cv2.INTER_NEAREST)
 
     # Imprimimos el tamaño del frame redimensionado
     len_resized_frame = len(frame.tobytes())
@@ -43,15 +76,31 @@ while True:
         len_resized_frame_old = len_resized_frame
 
     # Display
-    # cv2.imshow("frame", frame)
-    # if cv2.waitKey(1) == ord('q'):
-    #     break
+    if display:
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
 
     # Enviamos el frame por el socket en bloques de tamaño máximo permitido
     message = frame.tobytes(order='C')
     block_size = 65000  # Tamaño máximo de cada bloque
+    sock.sendto(message[:10], ('localhost', 8554))
+    print("Send start")
+    send = 0
     for i in range(0, len(message), block_size):
-        sock.sendto(message[i:i + block_size], ('127.0.0.1', 8554))
+        sock.sendto(message[i:i + block_size], ('localhost', 8554))
+        send += len(message[i:i + block_size])
+        print(f"Send: {send} / {len(message)}")
+        time.sleep(0.01)
+    print(send)
+    sock.sendto(message[:20], ('localhost', 8554))
+    print("Send end")
+    
+    # If user press type 'q' into the console in non blocking mode, exit
+    if input_thread.get_data() is not None and input_thread.get_data().strip() == 'q':
+        print("Se ha ha parado por el usuario")
+        input_thread.clear_data()
+        break
 
 # Cerramos el socket y la cámara
 sock.close()
